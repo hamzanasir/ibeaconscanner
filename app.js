be956bc2f812942c579c0ce33fd3ba0f4f2d86e3
Hamza Nasir <mnasir@hawk.iit.edu>
2018-10-08 01:51:33 -0500

    Convert rssi to distance in meters

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const BeaconScanner = require('node-beacon-scanner');
 const scanner = new BeaconScanner(); // Set an Event handler for beacons
 scanner.onadvertisement = (ad) => {
   console.log(JSON.stringify(ad, null, ' '));
+  console.log(getDistance(ad));
 };
 // Start scanning 
 scanner.startScan().then(() => {
@@ -9,3 +10,16 @@ scanner.startScan().then(() => {
 }).catch((error) => {
   console.error(error);
 });
+function getDistance(beacon) {
+    if (beacon.rssi === 0) {
+      return -1.0;
+    }
+    var ratio = beacon.rssi * 1.0 / beacon.iBeacon.txPower;
+    if (ratio < 1.0) {
+        return Math.pow(ratio, 10);
+    }
+    else {
+        var accuracy = (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
+        return accuracy;
+    }
+}
